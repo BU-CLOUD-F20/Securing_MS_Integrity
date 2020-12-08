ebc2a90aacc894813e3be1b39d4e24d983659135
noracnr <chenningrong@evocolabs.com>
2020-12-09 06:33:24 +0800

    update files for transformation

--- README.md
+++ README.md
@@ -1,130 +1,39 @@
-** **
+# Transformation
+    
+    Change all files from developers to intoto version: modification and creation.
 
-## Securing Integrity of Micro-Service Builds on Cloud (SIMS) Project Proposal
+## General files (code and necessary files)
+* standarize.py
+* task_clone_in_toto.yaml
+* developers' files for their project (like tasks, pipeline, pipelinerun)
+* a owner.json file for assign keys and missions.
 
-## Team Members:
-Role | Name | Email
------|------|------
-Mentor/Client | Shripad J Nadgowda | nadgowda@us.ibm.com
-Developer | Yanyu Zhang | zhangya@bu.edu
-Developer | Ningrong Chen | noracnr@bu.edu
-Developer | Tony Mark | marktony@bu.edu
-Developer | Staratzis Dimitrios | dstara@bu.edu
-Developer | Zhou Fang | fzx2666@bu.edu
+## A simple example
 
-## 1. Vision and Goals Of The Project:
+##### origin files(in tranformation-before)
+* task-pytest.yaml
+* task-clone.yaml
+* pytest-pipeline.yaml
+* pvc-claim.yaml
+* pipelinerun.yaml
+* owner.json
 
+##### After run `python3 standarize.py`
 
-CI/CD pipelines are very commonly used today, especially in the industry.
-Companies use them to enable their developers to write and immediately it into a testing environment. 
-This environment is called a pipeline. 
-Pipelines usually run on clusters, for better performance. 
-Out of all pipelines, we chose to work with Tekton, first introduced by Google.
-The main advantage of Tekton is that it can run on the same Kubernetes cluster as the program that is tested. 
-Other pipelines need to be configured in a different Kubernetes cluster. 
-A pipeline consists of tasks and each task consists of a number of tests.
-A newly pushed code that passes all tasks can be immediately deployed in the main branch of the application.
-However, there is no known software today that automatically verifies the integrity and the correct execution of each task.
-In this project, we introduce SIMS, a verifier for all steps of a pipeline that ensures integrity. 
-Each step of every task is signed by the project developer in an automated way. To do that, we use in-toto, developed by NYU.
+##### new files
+1. modified from original ones
 
+- intoto-task-pytest.yaml  <--task-pytest.yaml
+- intoto-task-clone.yaml   <--task-clone.yaml
+- intoto-pytest-pipeline.yaml  <- pytest-pipeline.yaml
+- intoto-pipelinerun.yaml   <-- pipelinerun.yaml
 
-## 2. Users/Personas Of The Project
+2. create new files for intoto
 
-* Our verifier will be used by company researchers and individuals who usually use cloud platforms to run tasks in CI/CD pipeline. It can be used in all major Operating Systems, including Windows, Mac, and Linux.
-
-* This tool will be useful for clients who need to make sure that the application they are interested in has undergone sufficient testing.
-We have not yet concluded how clients will have access to the result of our verifier.  
-
-* Micro-service applications are being developed by multiple and distributed teams. SIMS provides a framework to establish trust between these teams and ensure secure collaboration.
-
-* This project will not target the correctness of the pipeline itself. We are assuming the pipeline works perfectly. We only care about the integrity of each step.
-
-
-## 3. Scope and Features Of The Project:
-
-* Users should be able to use SIMS easily. The output should provide a detailed report. 
- 
-* Users will also be able to see a log of previous pipeline executions
-
-* Users will be able to manually check the signature applied to the executed tests.
-
-* The computational overhead of SIMS should be minimal in comparison to the pipeline cost.
-
-## 4. Solution Concept
-
-Assuming a software application that uses the Tekton pipeline:
-
-* Whenever a developer pushes a new code, the new version of the application is transferred to the pipeline. 
-
-* All tasks of the pipeline begin execution. As we mentioned before, each task can have many steps. After successful execution, each step is granted an automatic signature. All proceeding steps will execute after that. At this point, we should mention that we are going to enable users, to apply signatures both after each step or after each task, depending on their application characteristics.
-
-* At the end of the pipeline, a new final-task, created by SIMS will collect all previous signatures and decide about the integrity of the system.
-
-![alt text](https://github.com/BU-CLOUD-F20/Securing_MS_Integrity/blob/master/Images/Flowchart.jpeg)
-
-## 5. Acceptance criteria
-
-SIMS is a simple project which evaluates the pipeline steps of a CI/CD application.
-
-The user should be able to:
-
-* Receive the pipeline evaluation without a significant penalty in execution time for using SIMS.
-
-* Be able to select between applying the in-toto signatures in every task or every step.
-
-* Be able to easily locate possible issues and their cause.
-
-## 6. Release Planning
-
-Release #1 (due by Oct.1):
-
-- Presentation of the basic structure of our system.
-- Demo the basic Tekton pipeline and In-toto tutorial.
-
-Release #2 (due by Oct.15): 
-
-- In-toto and Tekton running side by side as two different entities in a demo example.
-
-Release #3 (due by Oct.29):
-
-- First effort to combine the two tools.
-
-Release #4 (due by Nov.12):
-
-- Hopefully a functional implementation of our project.
-
-Release #5 (due by Dec.3):
-
-- Final version.
-
-## 7. Execution Instructions
-
-### Kubernetes
-export KUBECONFIG=<kubeconfig_file>
-
-For all tasks:
-kubectl apply -f <task-file>.yaml
-
-For the pipeline:
-kubectl apply -f <pipeline-file>.yaml
-
-For the pipeline run:
-kubectl apply -f <pipeline-run-file>.yaml
-
-To run the dashboard:
-kubectl proxy
-
-To launch the dashboard:
-http://localhost:8001/api/v1/namespaces/tekton-pipelines/services/tekton-dashboard:http/proxy/
-
-### In-toto
-
-- **```in-toto-run```** : It is used to execute a step in the software supply chain. This can be anything relevant to the project such as tagging a release with ```git```, running a test, or building a binary. 
-
-- **```in-toto-record```** : It works similar to ```in-toto-run``` but can be used for multi-part software supply chain steps, i.e. steps that are not carried out by a single command. 
-
-- **```in-toto-verify```** : Verify on the final product.
-
-- **```in-toto-sign```** : It is a metadata signature helper tool to add, replace, and verify signatures within in-toto Link or Layout metadata.
-
+- task-clone-in-toto.yaml   
+    - for  cloning intoto things 
+    - only add workspace dynamically by origin task.
+- intoto-task-create-layout.yaml 
+    - for creating intoto layout depending on owner.json
+- intoto-task-verify.yaml  
+    - for verifying whole project.
--- transformation_after/standarize.py
+++ transformation_after/standarize.py
@@ -2,6 +2,7 @@ import os
 from enum import Enum
 import re
 import json
+import yaml
 
 class Type(Enum):
   Task = 1
@@ -11,12 +12,13 @@ class Type(Enum):
   PersistentVolumeClaim = 5
   PipelineResource = 6
 
-fields = ['apiVersion','kind','metadata','spec','steps','stepTemplate','description','params','resources','inputs','outputs','workspaces','results','volumes','volumeMounts','stepTemplate','sidecars','name','namespace','type','default','image','args','command','volumeMounts','mountPath','emptyDir','taskRef','tasks','script','timeout','value','resources','env','targetPath','securityContext','privileged','workingDir','hostPath','configMap','valueFrom','secretKeyRef','key','runAsUser','generateName','podTemplate','runAsNonRoot','taskSpec']
-
+fields = ['apiVersion','kind','metadata','spec','steps','stepTemplate','params','resources','inputs','outputs','workspaces','results','volumes','volumeMounts','stepTemplate','sidecars','namespace','type','default','image','args','command','volumeMounts','mountPath','emptyDir','taskRef','tasks','script','timeout','value','resources','env','targetPath','securityContext','privileged','workingDir','hostPath','configMap','valueFrom','secretKeyRef','key','runAsUser','generateName','podTemplate','runAsNonRoot','taskSpec']
+workspaces = {}
+taskname_ori_modi = {}
 # Transfer existing file to intoto version
 def standarize():
   # Read users' json
-  jsonfile =  open('tasks_commands.json','r')
+  jsonfile =  open('owner.json','r')
   commands = json.loads(jsonfile.read())
   jsonfile.close()
   # Deal with all yamls
@@ -27,8 +29,8 @@ def standarize():
       continue 
     with open(os.path.join(os.getcwd(), filename),'r') as f:
       Lines = f.readlines()
-      print(filename)
-      
+      print("start standarize :"+filename)
+      f.close()
       filetype = 0
       for line in Lines:
         if line.startswith("kind"):
@@ -48,10 +50,23 @@ def standarize():
             print(line)
       # When file type is task, modify to intoto version if need
       if filetype == Type.Task:
+        print("  Origin Task",filename)
         modify_flag = False
         startflag_metadata = False
         origin_name =""
+        startflag_workspace = False
         for idx,line in enumerate(Lines):
+          if "workspace" in line:
+            startflag_workspace = True
+          if startflag_workspace:
+              if "name:" in line:
+                temp_list_workspaces = line.rstrip().split("name:")
+                workspaces["name"] = temp_list_workspaces[1].strip()
+              if "mountPath:" in line:
+                temp_list_workspaces = line.rstrip().split("mountPath:")
+                workspaces["mountPath"] = temp_list_workspaces[1].strip()
+              if "name" in workspaces and "mountPath" in workspaces:
+                startflag_workspace = False
           if "metadata" in line:
             startflag_metadata = True
           if startflag_metadata:
@@ -60,15 +75,18 @@ def standarize():
               origin_name = temp_list[1].strip()
               startflag_metadata = False
             # TODO: Deal with situation that no name field in metadata
+        if startflag_workspace:
+          raise Exception("No Valid Field: Workspaces dont have name or mouthPath")
         # check if the file need to modify or not
         if origin_name == '':
-          raise FileError("No Valid Field:Task file's lack of metadata-name field")
-        else:
-          print("origin_name: ",origin_name)
-        print(commands)
+          raise Exception("No Valid Field:Task file's lack of metadata-name field")
+        # else:
+        #   print("origin_name: ",origin_name)
+        #print("commands:", commands)
         key_owner = ""
         step_name = ""
         command = ""
+        # When task's name is mentioned in json, modify it.
         for key, value in commands.items():
           # TODO: when value is empty
           for k,v in value.items():
@@ -79,6 +97,7 @@ def standarize():
               modify_flag = True
         if modify_flag:
           # Create a new Task by adding intoto part
+          print("  modify to ","intoto-"+filename)
           newTaskFile = open("intoto-"+filename,'w')
           argsLines = []
           startflag_args = False
@@ -108,7 +127,7 @@ def standarize():
               break
           # deal with argsLines
           indent_spaces = ''
-          print(argsLines)
+          #print(argsLines)
           for idx,line in enumerate(argsLines):
             tmp = line.strip()
             if ":" in tmp:
@@ -143,19 +162,138 @@ def standarize():
               line +="\n"
               newTaskFile.write(line)
               break
-          print(argsLines)
+          #print(argsLines)
           # write rest of lines in output file
           for line in Lines[idx_args:]:
             newTaskFile.write(line)
           newTaskFile.close()
-          # create new tasks:lone-in-toto
-      #     TaskCloneIntoto = open("task-clone-in-toto.yaml",'w')
           
-      # # Add intoto tasks
-      # if filetype == Type.Pipeline:
-      #   continue
-      # # Add relavant params about intoto tasks
-      # if filetype == Type.PipelineRun:    
+      
+      if filetype == Type.Pipeline:
+        print("  Origin Pipeline ",filename)
+        # 1. insert params in spec
+        startflag_spec = False
+        startflag_spec_params = False
+        idx_spec = 0
+        contents = Lines
+        # find last index of params in spec
+        for idx,line in enumerate(Lines):
+          temp_list = line.split(":")
+          if len(temp_list) > 1:
+            field_line = temp_list[0].strip()
+            if startflag_spec and startflag_spec_params and field_line in fields:
+              startflag_spec = False
+              startflag_spec_params = False
+          if "spec" in line:
+            startflag_spec = True
+          if startflag_spec and not startflag_spec_params:
+            if "params:" in line:
+              startflag_spec_params = True
+          if startflag_spec and startflag_spec_params:
+            idx_spec = idx
+        # Insert 
+        newParams = "    - name: in-toto-repo\n      description: the in-toto repo\n"
+        newParams += "    - name: in-toto-branch\n      description: in-toto-branch\n"
+        newParams += "    - name: in-toto-directory\n      description: directory to clone the in-toto repo\n"
+        contents.insert(idx_spec,newParams)
+        #2. insert intoto tasks in tasks based on modified contents
+        startflag_tasks = False
+        startflag_taskRef= False
+        idx_tasks=-1
+        idx_runAfter = -1
+        initial_tasks = []
+        initial_tasks_str = ""
+        for key, value in commands.items():
+          # TODO: when value is empty
+          for k,v in value.items():
+            taskname_ori_modi[k] = k.join("-intoto")
+        #print(taskname_ori_modi)
+        for idx, line in enumerate(contents):
+          temp_list = line.split(":")
+          if len(temp_list) > 1:
+            field_line = temp_list[0].strip()
+            if startflag_tasks and startflag_taskRef and field_line in fields:
+              startflag_taskRef = False
+          if startflag_tasks:
+            if "taskRef:" in line:
+              startflag_taskRef = True
+            if startflag_taskRef:
+              if "name:" in line:
+                temp_list_name =line.split(":")
+                origin_taskname = temp_list_name[1].strip()
+                if origin_taskname in taskname_ori_modi:
+                  line = line.rstrip()+"-intoto\n"
+            initial_tasks.append(line)
+            initial_tasks_str += line
+          if "tasks:" in line:
+            startflag_tasks = True
+            idx_tasks = idx+1
+        #print("initial_tasks",initial_tasks)
+        json_d = yaml.safe_load(initial_tasks_str)
+        last_task = json_d[len(json_d)-1]
+        last_taskname = last_task["name"]
+        for idx, line in enumerate(initial_tasks):
+          if "taskRef:" in line:
+            #print(line,idx_runAfter)
+            if idx_runAfter == -1:
+              idx_runAfter = idx
+        if idx_runAfter != -1:
+          initial_tasks.insert(idx_runAfter,"      runAfter:\n        - create-in-toto-layout\n")
+        else :
+          raise Exception("Some error when inserting runAfter")
+        #print("initial_tasks",initial_tasks)
+        before_tasks = "    - name: in-toto-clone\n      taskRef:\n       name: task-in-toto-clone\n      workspaces:\n        - name: artifacts\n          workspace: artifacts\n      params:\n        - name: repository\n          value: $(params.in-toto-repo)\n        - name: branch\n          value: $(params.in-toto-branch)\n        - name: git-user\n          value: $(params.git-user)\n        - name: git-password\n          value: $(params.git-password)\n        - name: directory-name\n          value: $(params.in-toto-directory)\n"
+        before_tasks+= "\n    - name: create-in-toto-layout\n      runAfter:\n        - in-toto-clone\n      taskRef:\n        name: task-create-layout\n      workspaces:\n        - name: artifacts\n          workspace: artifacts\n      params:\n        - name: directory-name\n          value: $(params.in-toto-directory)\n\n"
+        after_tasks= "\n    - name: input-verification\n      runAfter:\n        - "+last_taskname+"\n      taskRef:\n        name: task-verify\n      workspaces:\n        - name: artifacts\n          workspace: artifacts\n      params:\n        - name: directory-name\n          value: $(params.in-toto-directory)"
+        if idx_tasks != -1:
+          contents = contents[:idx_tasks]
+          contents.append(before_tasks)
+
+          contents += initial_tasks
+          contents.append(after_tasks)
+        else:
+          raise Exception("No tasks in pipeline")
+        # add runAfter
+        print("  modify to ","intoto-"+filename)
+        f_write = open("intoto-"+filename,'w')
+        contents = "".join(contents)
+        f_write.write(contents)
+        f_write.close()
+
+      # Add relavant params about intoto tasks
+      if filetype == Type.PipelineRun:
+        print("  Origin PipelineRun ",filename) 
+        idx_spec_run = -1
+        startflag_spec_run = False
+        startflag_spec_params_run = False
+        contents_run = Lines
+        for idx,line in enumerate(Lines):
+          temp_list = line.split(":")
+          if len(temp_list) > 1:
+            field_line = temp_list[0].strip()
+            if startflag_spec_run and startflag_spec_params_run and field_line in fields:
+              startflag_spec_run = False
+              startflag_spec_params_run = False
+          if "spec:" in line:
+            startflag_spec_run = True
+          if startflag_spec_run and not startflag_spec_params_run:
+            if "params:" in line:
+              startflag_spec_params_run = True
+          if startflag_spec_run and startflag_spec_params_run:
+            idx_spec_run = idx
+        if idx_spec_run != -1:
+          newParams = "    - name: in-toto-repo\n      value: https://github.com/BU-CLOUD-F20/Securing_MS_Integrity"
+          newParams += "\n    - name: in-toto-branch\n      value: Zhou"
+          newParams += "\n    - name: in-toto-directory\n      value: in-toto\n"
+          contents_run.insert(idx_spec_run,newParams)
+          print("  modify to ","intoto-"+filename)
+          f_write = open("intoto-"+filename,'w')
+          contents_run = "".join(contents_run)
+          f_write.write(contents_run)
+          f_write.close()
+        else:
+          raise Exception("no spec param in pipelinerun")
+
       #   continue
 
 # create new tasks:create-layout,verify
@@ -168,7 +306,7 @@ def createTasks():
       continue 
     with open(os.path.join(os.getcwd(), filename),'r') as f:
       Lines = f.readlines()
-      print(filename)
+      #print(filename)
       filetype = 0
       for line in Lines:
         if line.startswith("kind"):
@@ -189,15 +327,16 @@ def createTasks():
               startflag_metadata = False
             # TODO: Deal with situation that no name field in metadata
         if origin_name == '':
-          raise FileError("No Valid Field:Task file's lack of metadata-name field")
-          continue
+          raise Exception("No Valid Field:Task file's lack of metadata-name field")
         # Create a new Task by adding intoto part
+        print("create intoto-task-verify.yaml")
         TaskVerify = open("intoto-task-verify.yaml",'w')
+        print("create intoto-task-create-layout.yaml")
         TaskCreateLayout = open("intoto-task-create-layout.yaml",'w')
         argsLines_TaskVerify = []
         startflag_args = False
         startflag_stepsname = False
-        #TaskVerify: change name and split args part
+      #TaskVerify: change name and split args part
         for idx,line in enumerate(Lines):
           if startflag_args:
             argsLines_TaskVerify.append(line)
@@ -231,7 +370,7 @@ def createTasks():
             idx_args = idx
             break
         argsLines_TaskCreateLayout = []
-        # TaskCreateLayout: change names
+      # TaskCreateLayout: change names
         for idx,line in enumerate(Lines):
           if startflag_args:
             argsLines_TaskCreateLayout.append(line)
@@ -264,10 +403,10 @@ def createTasks():
             startflag_args = False
             idx_args = idx
             break
-        # deal with argsLines
+      # deal with argsLines
         indent_spaces = ''
-        print(argsLines_TaskVerify)
-        print(argsLines_TaskCreateLayout)
+        #print(argsLines_TaskVerify)
+        #print(argsLines_TaskCreateLayout)
         for idx,line in enumerate(argsLines_TaskVerify):
           tmp = line.strip()
           if ":" in tmp:
@@ -295,18 +434,33 @@ def createTasks():
             clone_line = line+"python create_layout.py\n"
             TaskCreateLayout.write(clone_line)
             break
-        print(argsLines_TaskVerify)
+        #print(argsLines_TaskVerify)
         # write rest of lines in output file
         for line in Lines[idx_args:]:
           TaskVerify.write(line)
           TaskCreateLayout.write(line)
         TaskVerify.close()
         TaskCreateLayout.close()
+        #print("结束"+filename)
         break
+        
+  # TaskCloneIntoto: append workspace (only run once)
+  flag_TaskCloneIntoto = True
+  TaskCloneIntotoR = open("task-clone-in-toto.yaml",'r')
+  for l in TaskCloneIntotoR.readlines():
+    if "workspaces:" in l:
+      flag_TaskCloneIntoto = False
+  if flag_TaskCloneIntoto:
+    TaskCloneIntoto = open("task-clone-in-toto.yaml",'a+')
+    new_workspaces = "  workspaces:\n    - name: "+workspaces["name"]+"\n      mountPath: "+workspaces["mountPath"]+"\n"
+    TaskCloneIntoto.write(new_workspaces)
+
 
 
 
 if __name__ == "__main__":
     # execute only if run as a script
+    print("---Transformation:---")
     standarize()
     createTasks()
+    print("---END---")
